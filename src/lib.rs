ce8d02503409fe9d4fd3444e4ea01dbf785db58e
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-07-14 21:05:36 +0200

    Some stress tests
    Tests doing a lot of work from bunch of threads, to see if something
    breaks.

--- src/lib.rs
+++ src/lib.rs
@@ -262,6 +262,9 @@ impl<'a, T: RefCnt> Guard<'a, T> {
         res
     }
 
+    // Note on the lifetime: we can't really return the 'a lifetime even though the underlying
+    // arc-swap lives that long. The arc-swap does not guarantee the lifetime of the Arc inside,
+    // the guard does.
     pub fn get_ref<'g>(guard: &'g Self) -> Option<&'g T::Base> {
         unsafe { guard.ptr.as_ref() }
     }
