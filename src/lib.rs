8eab22f11bf0eb8ce3a11b19042a77f03306a1f0
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-10-15 23:03:21 +0200

    Version bump to 0.3.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![doc(
-    html_root_url = "https://docs.rs/arc-swap/0.3.1/arc-swap/",
+    html_root_url = "https://docs.rs/arc-swap/0.3.2/arc-swap/",
     test(attr(deny(warnings)))
 )]
 #![deny(missing_docs, warnings)]
