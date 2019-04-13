435beeb665d2eb91e930ef6b249171ad3912e06d
Michal 'vorner' Vaner <vorner@vorner.cz>
2019-04-13 21:24:02 +0200

    Proof comment update
    There were some editing artifacts, like paragraphs out of order. The
    proof (and the code) stays the same.

--- src/lib.rs
+++ src/lib.rs
@@ -290,23 +290,18 @@ pub use ref_cnt::{NonNull, RefCnt};
 //
 // The first one is based on hazard pointers idea, but slightly modified. There's a global
 // repository of pointers that owe a reference. When someone swaps a pointer, it walks this list
-// and pays all the debts (and takes them).
+// and pays all the debts (and takes them out of the repository).
 //
-// If storing into the repository fails (because the thread used up all its own slots, or because
-// the pointer got replaced in just the wrong moment and it can't confirm the reservation), unlike
-// the full hazard-pointers approach, we don't retry, but fall back onto secondary strategy.
+// For simplicity and performance, storing into the repository is fallible. If storing into the
+// repository fails (because the thread used up all its own slots, or because the pointer got
+// replaced in just the wrong moment and it can't confirm the reservation), unlike the full
+// hazard-pointers approach, we don't retry, but fall back onto secondary the strategy.
 //
 // Each reader registers itself so it can be tracked, but only as a number. Each writer first
 // switches the pointer. Then it takes a snapshot of all the current readers and waits until all of
 // them confirm bumping their reference count. Only then the writer returns to the caller, handing
 // it the ownership of the Arc and allowing possible bad things (like being destroyed) to happen to
 // it. This has its own disadvantages, so it is only the second opportunity.
-//
-// Because bumping the reference count can cause contention and slows things down, there's another
-// way to track missing reference counts. There's a global registry of debts and each lease of a
-// missing reference count is written there. If someone wants to replace the pointer inside, it
-// traverses the whole registry and pays all the debts (by incrementing the reference counts) ‒
-// this avoids paying the debt in the most common read only cases.
 //
 // # Unsafety
 //
@@ -360,7 +355,7 @@ pub use ref_cnt::{NonNull, RefCnt};
 //
 // We have AcqRel (well, SeqCst, but that's included) on the swap and Acquire on the loads. In case
 // of the double read around the debt allocation, we do that on the *second*, because of ABA.
-// That's also why that SeqCst on the allocation itself is not enough.
+// That's also why that SeqCst on the allocation of debt itself is not enough.
 //
 // ## The generation lock
 //
@@ -387,7 +382,8 @@ pub use ref_cnt::{NonNull, RefCnt};
 // synchronized through Release on creation and Acquire on load on the head pointer.
 //
 // The debts work similar to locks ‒ Acquire and Release make all the pointer manipulation at the
-// interval where it is written down.
+// interval where it is written down. However, we use the SeqCst on the allocation of the debt for
+// the same reason we do so with the generation lock.
 //
 // In case the writer pays the debt, it sees the new enough data (for the same reasons the stale
 // zeroes are not seen). The reference count on the Arc is AcqRel and makes sure it is not
